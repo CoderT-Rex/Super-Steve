219bb615f98edecfb492e4c7ef86d3006f554595
CoderT-Rex <joshua_rex@rocketmail.com>
2025-08-31 19:43:22 -0400

    creating a seperate project using my python asteroids game as a base

---
+++ main.py
@@ -0,0 +1,63 @@
+import sys
+import pygame
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from constants import *
+from shot import Shot
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    Shot.containers = (shots, updatable, drawable)
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        updatable.update(dt)
+        
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+               print("Game over!")
+               sys.exit()
+
+        for shot in shots:
+            for asteroid in asteroids:
+                if asteroid.collision(shot):
+                    shot.kill()
+                    asteroid.split()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+        
+        dt = clock.tick(60)/1000
+
+    print("Game Over")
+
+
+if __name__ == "__main__":
+    main()
+
+
